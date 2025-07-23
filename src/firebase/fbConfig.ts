418225704686749004e57c76b7d44a8ac3dceaa6
Tyler Wronski <wronskityler@gmail.com>
2025-07-23 12:42:24 -0700

    Added Firebase and Firestore, started Registration and Login/Logout components

---
+++ src/firebase/fbConfig.ts
@@ -0,0 +1,20 @@
+// Firebase and Firestore config 
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import type { Auth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCirDCbMtYVQuQNEpGhG9iUq0qsf9WV3eM",
+  authDomain: "e-commerce-app-3b04f.firebaseapp.com",
+  projectId: "e-commerce-app-3b04f",
+  storageBucket: "e-commerce-app-3b04f.firebasestorage.app",
+  messagingSenderId: "300185760890",
+  appId: "1:300185760890:web:29d0bf242a8d42814977c3"
+};
+
+const app = initializeApp(firebaseConfig); 
+const auth: Auth = getAuth(app); 
+const db = getFirestore(app); 
+
+export { auth, db }; 
