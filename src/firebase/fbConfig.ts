81058acdc59eb3408006b043c30fd2c918eb7e90
Tyler Wronski <wronskityler@gmail.com>
2025-07-27 09:57:08 -0700

    Started Order management, order interface, order details

--- src/firebase/fbConfig.ts
+++ src/firebase/fbConfig.ts
@@ -4,7 +4,6 @@ import { getAuth } from "firebase/auth";
 import type { Auth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-/*
 const firebaseConfig = {
   apiKey: "your_api_key",
   authDomain: "your_auth_domain",
@@ -12,16 +11,6 @@ const firebaseConfig = {
   storageBucket: "your_storage_bucket",
   messagingSenderId: "your_messaging_sender_id",
   appId: "your_app_id",
-};
-*/
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCirDCbMtYVQuQNEpGhG9iUq0qsf9WV3eM",
-  authDomain: "e-commerce-app-3b04f.firebaseapp.com",
-  projectId: "e-commerce-app-3b04f",
-  storageBucket: "e-commerce-app-3b04f.firebasestorage.app",
-  messagingSenderId: "300185760890",
-  appId: "1:300185760890:web:29d0bf242a8d42814977c3"
 };
 
 const app = initializeApp(firebaseConfig); 
